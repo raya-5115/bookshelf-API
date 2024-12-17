ffb52f2b4e95487146adef5fc46faaf199e19458
raya-5115 <naufalrayapragata@gmail.com>
2024-12-17 15:22:10 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const { addBookHandler, getAllBooksHandler, getBooksbyIdHandler, editBookByIdHandler, deleteBookByIdHandler } = require('./handler.js');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksbyIdHandler
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookByIdHandler
+  },
+
+];
+
+module.exports = routes;
